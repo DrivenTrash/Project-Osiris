204b717c1870bf95e3d40564bd8d68738ba8d667
Valentin Gerdon <valentin.gerdon@porsche.de>
2024-02-09 11:30:17 +0100

    Overload toStrinf

--- Creature.py
+++ Creature.py
@@ -1,9 +1,14 @@
 from random import randint
 
 class Creature:
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.moves = []
         for i in range(100):
             self.moves.append(randint(1, 9))
+
+    def __str___(self):
+        return self.name
+    
     def move(self):
         return self.moves[randint(0, 99)]
