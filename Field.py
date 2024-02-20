b8297b5bfdabed573eb94221e026b0ccaa405ed9
Valentin Gerdon <valentin.gerdon@porsche.de>
2024-02-20 11:29:59 +0100

    add move_creature

--- Field.py
+++ Field.py
@@ -14,11 +14,31 @@ class Field:
         x = randint(0, self.x-1)
         y = randint(0, self.y-1)
         if self.field[x][y] == 0:
-            newCreature = Creature(self.generation, self.creatureCounter) 
+            newCreature = Creature(self.generation, self.creatureCounter, x, y) 
             self.field[x][y] = newCreature
             self.population.append(newCreature)
             self.creatureCounter += 1
     
+    def move_creature(self, creature):
+        x_old = creature.x
+        y_old = creature.y
+        move = creature.move()
+        x_new = x_old
+        y_new = y_old
+        if move%3 == 0:
+            x_new -= 1
+        elif move%3 == 2:
+            x_new += 1
+        if move//3 == 0:
+            y_new -= 1
+        elif move//3 == 2:
+            y_new += 1
+        if x_new >= 0 and x_new < self.x and y_new >= 0 and y_new < self.y and self.field[x_new][y_new] == 0:
+            self.field[x_old][y_old] = 0
+            self.field[x_new][y_new] = creature
+            creature.x = x_new
+            creature.y = y_new
+
     def display(self):
         for row in self.field:
             line = ""
