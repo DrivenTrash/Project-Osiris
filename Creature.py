f0a9dee15ca44e81d0e6ef2169c42551fdc25f4e
Valentin Gerdon <valentin.gerdon@porsche.de>
2024-02-20 11:29:27 +0100

    Added Position

--- Creature.py
+++ Creature.py
@@ -1,13 +1,16 @@
 from random import randint
 
 class Creature:
-    def __init__(self, generation, number):
+    def __init__(self, generation, number, x, y):
         self.generation = generation
         self.number = number
-        self.genome = [randint(1, 9) for i in range(100)]
+        self.x = x
+        self.y = y
+        self.genome = [randint(0, 8) for i in range(100)]
 
     def __str__(self):
         return "c" + str(self.number)
     
     def move(self):
         return self.genome[randint(0, 99)]
+    
