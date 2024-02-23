3925baa8523781c0129acea6f141ca532bccfdbf
Valentin Gerdon <valentin.gerdon@porsche.de>
2024-02-23 10:50:36 +0100

    Unittest for move_creature

--- test_field.py
+++ test_field.py
@@ -1,16 +1,62 @@
-import unittest
-from unittest.mock import patch
+import pytest
 from field import Field
 from creature import Creature
 
 ##test the move_creature method
-class TestMoveCreature(unittest.TestCase):
-    
-    @patch('creature.Creature.move', return_value=0)
-    def test_move_0(self, mock_move): 
-        test = Field(5, 5)
-        c0 = Creature(0, 0, 2, 2)
-        test.field[2][2] = c0
-        test.move_creature(c0)
-        self.assertEqual(test.field[2][2], 0)
-        self.assertEqual(test.field[1][2], c0)
+@pytest.fixture(params=[0, 1, 2, 3, 4, 5, 6, 7, 8])
+def mock_move(request, monkeypatch):
+    def mock_move(self):
+        return request.param
+    monkeypatch.setattr(Creature, "move", mock_move)
+    return request.param
+
+def test_move(mock_move): 
+    test = Field(5, 5)
+    cTest = Creature(0, 0, 2, 2)
+    test.field[2][2] = cTest
+    test.move_creature(cTest)
+    if mock_move == 0:
+        assert test.field[2][2] == 0
+        assert cTest.x == 1
+        assert cTest.y == 1
+        assert test.field[1][1] == cTest
+    if mock_move == 1:
+        assert test.field[2][2] == 0
+        assert cTest.x == 2
+        assert cTest.y == 1
+        assert test.field[1][2] == cTest
+    if mock_move == 2:
+        assert test.field[2][2] == 0
+        assert cTest.x == 3
+        assert cTest.y == 1
+        assert test.field[1][3] == cTest
+    if mock_move == 3:
+        assert test.field[2][2] == 0
+        assert cTest.x == 1
+        assert cTest.y == 2
+        assert test.field[2][1] == cTest
+    if mock_move == 4:
+        assert test.field[2][2] != 0
+        assert cTest.x == 2
+        assert cTest.y == 2
+        assert test.field[2][2] == cTest
+    if mock_move == 5:
+        assert test.field[2][2] == 0
+        assert cTest.x == 3
+        assert cTest.y == 2
+        assert test.field[2][3] == cTest
+    if mock_move == 6:
+        assert test.field[2][2] == 0
+        assert cTest.x == 1
+        assert cTest.y == 3
+        assert test.field[3][1] == cTest
+    if mock_move == 7:
+        assert test.field[2][2] == 0
+        assert cTest.x == 2
+        assert cTest.y == 3
+        assert test.field[3][2] == cTest
+    if mock_move == 8:
+        assert test.field[2][2] == 0
+        assert cTest.x == 3
+        assert cTest.y == 3
+        assert test.field[3][3] == cTest
